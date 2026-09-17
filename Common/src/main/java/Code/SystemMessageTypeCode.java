220f97818335fc847cb3e9792689453f7507ab94
fire_tree <w_work2026@163.com>
2026-09-17 09:24:11 +0800

    使用vibe-coding写了一下Forum-Notification模块

---
+++ Common/src/main/java/Code/SystemMessageTypeCode.java
@@ -0,0 +1,9 @@
+package Code;
+
+public class SystemMessageTypeCode
+{
+
+    public static final short suscribe = 0;
+
+    public static final short text = 0;
+}
--- Common/src/main/java/Mapper/SystemMessageMapper.java
+++ Common/src/main/java/Mapper/SystemMessageMapper.java
@@ -4,9 +4,13 @@ package Mapper;
 import Entity.SystemMessage;
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 @Mapper
 public interface SystemMessageMapper extends BaseMapper<SystemMessage>
 {
-
+    //查询系统发给某个用户的最大会话内序号，用来在Redis缓存失效后恢复chatId的计数
+    @Select("select max(chat_id) from system_message where receiver_id = #{receiverId}")
+    public Long getMaxChatId(@Param("receiverId") Long receiverId);
 }
