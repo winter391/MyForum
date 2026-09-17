3f846b6f60fe490323a2e894cef187ce34ae4fd4
fire_tree <w_work2026@163.com>
2026-09-17 10:14:59 +0800

    新建了Forum-search模块

---
+++ Fourm-Search/src/main/java/fourmsearch/FourmSearchApplication.java
@@ -0,0 +1,13 @@
+package fourmsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourmSearchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FourmSearchApplication.class, args);
+    }
+
+}
---
+++ Fourm-Search/src/test/java/fourmsearch/FourmSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package fourmsearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourmSearchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
